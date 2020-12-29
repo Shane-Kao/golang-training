870d1343849ee28d95d0595ad27958bd1d03e24d
shanekao <shanekao19870615@gmail.com>
2020-12-29 21:40:47 +0800

    add data-var.go

---
+++ data-var.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println(3)
+	fmt.Println(3.1415)
+	fmt.Println("測試中文")
+	fmt.Println(true)
+	fmt.Println('a')
+	var x int
+	x=4
+	fmt.Println(x)
+	x=10
+	fmt.Println(x)
+	//x="Hello"
+	var f float64=3.1415
+	fmt.Println(f)
+	var s string="Hello Golang"
+	fmt.Println(s)
+	var test bool=true
+	fmt.Println(test)
+	var c rune='b'
+	fmt.Println(c)
+}
