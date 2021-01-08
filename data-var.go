7b1fb6c2fb7a1a8f0c821fffa7edc3cb053defc3
shanekao <shanekao19870615@gmail.com>
2021-01-08 20:42:18 +0800

    add op.go

--- data-var.go
+++ data-var.go
@@ -1,5 +1,8 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 func main(){
 	fmt.Println(3)
 	fmt.Println(3.1415)
@@ -20,4 +23,10 @@ func main(){
 	fmt.Println(test)
 	var c rune='b'
 	fmt.Println(c)
+	fmt.Println(reflect.TypeOf(c))
+	fmt.Println(reflect.TypeOf(test))
+	quantity:=4
+	fmt.Println(reflect.TypeOf(quantity))
+	fmt.Println(reflect.TypeOf(float64(quantity)))
+	fmt.Println(float64(quantity))
 }
